24d34c54ba5f25242fe2b73d8b034d76a387eea8
MrGago <cleamen@free.fr>
2022-01-26 17:16:50 +0100

    Wwise integration & Source control

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,50 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID TEST_TICK = 564845035U;
+    } // namespace EVENTS
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID GAME_VOLUME_AMBIANT = 1021071045U;
+        static const AkUniqueID GAME_VOLUME_MUSIC = 1039087204U;
+        static const AkUniqueID GAME_VOLUME_SFX = 1011990060U;
+        static const AkUniqueID GAME_VOLUME_VOICES = 2065406626U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID CORE = 3787826988U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID AMBIANT = 78669895U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SFX = 393239870U;
+        static const AkUniqueID VOICES = 3313685232U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
