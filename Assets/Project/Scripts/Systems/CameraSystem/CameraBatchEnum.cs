e83ad82baaf3abbe0aca0a83e3017be4ef7b5f92
ETU\q.roussel <quentinroussel1998@gmail.com>
2022-02-08 15:04:47 +0100

    Debug Chapter 3

--- Assets/Project/Scripts/Systems/CameraSystem/CameraBatchEnum.cs
+++ Assets/Project/Scripts/Systems/CameraSystem/CameraBatchEnum.cs
@@ -18,10 +18,16 @@ namespace TheFowler
     public enum CameraGenericKeyEnum
     {
         NULL,
+        
+        //Dialogue
         FACIAL_ROBYN,
         FACIAL_PHOEBE,
         FACIAL_ABI,
+        
+        //Explo
         TPS_CAMERA,
+        
+        //Combats
     }
 
     public static class CameraGenericKey
@@ -32,14 +38,21 @@ namespace TheFowler
             {
                 case CameraGenericKeyEnum.NULL:
                     break;
+                
+                //Dialogues
                 case CameraGenericKeyEnum.FACIAL_ROBYN:
                     return "Robyn_Facial";
                 case CameraGenericKeyEnum.FACIAL_PHOEBE:
                     return "Phoebe_Facial";
                 case CameraGenericKeyEnum.FACIAL_ABI:
                     return "Abigael_Facial";
+                
+                //Explo
                 case CameraGenericKeyEnum.TPS_CAMERA:
                     return "Robyn_TPS";
+                
+                //Combats
+                
                 default:
                     throw new ArgumentOutOfRangeException(nameof(key), key, null);
             }
