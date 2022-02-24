e429bc1accc83463f887948a6b6cc27c4840ceb9
ETU\q.roussel <quentinroussel1998@gmail.com>
2022-02-24 17:31:44 +0100

    Defend

---
+++ Assets/Project/Scripts/Systems/Spells/Effects/DefendEffect.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TheFowler
+{
+    public class DefendEffect : Effect
+    {
+        [SerializeField] private int effectDuration = 1;
+        [SerializeField, Range(0, 100)] private float effect = 25;
+        [SerializeField] private int manaToRestaure = 2;
+        
+        public enum RestaureManaAt
+        {
+            Cast,
+            Start_Turn
+        }
+
+        [SerializeField] private RestaureManaAt restaureManaAt = RestaureManaAt.Cast;
+        
+        public override IEnumerator OnBeginCast(BattleActor emitter, BattleActor[] receivers)
+        {
+            yield break;
+        }
+
+        public override IEnumerator OnCast(BattleActor emitter, BattleActor[] receivers)
+        {
+            switch (restaureManaAt)
+            {
+                case RestaureManaAt.Cast:
+                    emitter.Mana.AddMana(manaToRestaure);
+                    break;
+                case RestaureManaAt.Start_Turn:
+                    emitter.GetBattleComponent<Defense>().RestaureMana.AddListener(() => emitter.Mana.AddMana(manaToRestaure));
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+            emitter.GetBattleComponent<Defense>().DefendActor(effectDuration, effect);
+            yield break;
+        }
+
+        public override IEnumerator OnFinishCast(BattleActor emitter, BattleActor[] receivers)
+        {
+            yield break;
+        }
+    }
+}
