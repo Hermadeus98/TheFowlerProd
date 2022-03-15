e77f421e7558db3ea6e57bd659ce9424bad23052
ETU\q.roussel <quentinroussel1998@gmail.com>
2022-03-15 10:19:53 +0100

    VFX Robyn

---
+++ Assets/Project/Scripts/Helpers/MMFeedbacksExtension/MMPlayVisualEffect.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using MoreMountains.Feedbacks;
+using UnityEngine;
+using UnityEngine.VFX;
+
+namespace TheFowler
+{
+    [AddComponentMenu("")]
+    [FeedbackPath("TheFowler/Play Visual Effect")]
+    public class MMPlayVisualEffect : MMFeedback
+    {
+        [SerializeField] private VisualEffect VisualEffect;
+        
+        protected override void CustomPlayFeedback(Vector3 position, float feedbacksIntensity = 1)
+        {
+            VisualEffect.Play();
+        }
+    }
+}
---
+++ Assets/Project/Scripts/Systems/Spells/Effects/DamageEffectRobyn.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+namespace TheFowler
+{
+    public class DamageEffectRobyn : Effect
+    {
+        public float damage;
+        public VisualEffect VisualEffectAttack;
+        public float waitAttackEffect = 1f;
+        public VisualEffect VisualEffectAttackShock;
+        public float waitAttackEffectShock = 1f;
+        public ShockWave ShockWaveEffect;
+
+        public override void PreviewEffect(BattleActor emitter)
+        {
+            base.PreviewEffect(emitter);
+        }
+        
+        public override IEnumerator OnBeginCast(BattleActor emitter, BattleActor[] receivers)
+        {
+            yield break;
+        }
+
+        public override IEnumerator OnCast(BattleActor emitter, BattleActor[] receivers)
+        {
+            emitter.BattleActorAnimator.AttackCast();
+            SoundManager.PlaySound(audioEvent, emitter.gameObject);
+
+            yield return new WaitForSeconds(emitter.BattleActorAnimator.AttackCastDuration());
+
+            for (int i = 0; i < receivers.Length; i++)
+            {
+                var attackEffect = GameObject.Instantiate(VisualEffectAttack, receivers[i].transform.position, Quaternion.identity);
+                attackEffect.Play();
+            }
+
+            yield return new WaitForSeconds(waitAttackEffect);
+            
+            for (int i = 0; i < receivers.Length; i++)
+            {
+                var attackEffect = GameObject.Instantiate(VisualEffectAttackShock, receivers[i].transform.position, Quaternion.identity);
+                attackEffect.Play();
+                
+                var shockWave = GameObject.Instantiate(ShockWaveEffect, receivers[i].transform.position, Quaternion.identity);
+
+            }
+
+            foreach (var receiver in receivers)
+            {
+                var _damage = DamageCalculator.CalculateDamage(damage, emitter, receiver, ReferedSpell.SpellType, out var resistanceFaiblesseResult);
+
+                SoundManager.PlaySoundDamageTaken(receiver, resistanceFaiblesseResult);
+                
+                receiver.Health.TakeDamage(
+                    _damage
+                );
+            }
+            yield break;
+        }
+
+        public override IEnumerator OnFinishCast(BattleActor emitter, BattleActor[] receivers)
+        {
+            yield break;
+        }
+    }
+}
