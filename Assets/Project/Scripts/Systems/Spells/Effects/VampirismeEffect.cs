40f0f2f6d8beb2ab523e0b978ba687154606559f
ETU\q.roussel <quentinroussel1998@gmail.com>
2022-05-31 11:12:09 +0200

    Debug

--- Assets/Project/Scripts/Systems/Spells/Effects/VampirismeEffect.cs
+++ Assets/Project/Scripts/Systems/Spells/Effects/VampirismeEffect.cs
@@ -7,6 +7,7 @@ namespace TheFowler
     public class VampirismeEffect : Effect
     {
         public float damage;
+        public bool preserveEnemies = false;
 
         public override IEnumerator OnBeginCast(BattleActor emitter, BattleActor[] receivers)
         {
@@ -15,7 +16,7 @@ namespace TheFowler
 
         public override IEnumerator OnCast(BattleActor emitter, BattleActor[] receivers)
         {
-            var dmg = Damage(damage, emitter, receivers);
+            var dmg = Damage(damage, emitter, receivers, preserveEnemies);
             
             yield return new WaitForSeconds(SpellData.Instance.StateEffect_WaitTime);
             
